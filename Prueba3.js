1c66d1aeb52ea7a471437bcb17730bd4c868bbc9
FelipeMoba <134336278+FelipeMoba@users.noreply.github.com>
2024-01-14 12:04:34 -0500

    Add files via upload

---
+++ Prueba3.js
@@ -0,0 +1,27 @@
+[
+    {
+        "alumno": "Pedro",
+        "nota": 9.09,
+        "ranking": 10
+    },
+    {
+        "alumno": "Juan",
+        "nota": 4,
+        "ranking": 150
+    },
+    {
+        "alumno": "Ana",
+        "nota": 8.5,
+        "ranking": 25
+    },
+    {
+        "alumno": "Luis",
+        "nota": 6.75,
+        "ranking": 80
+    },
+    {
+        "alumno": "María",
+        "nota": 7.2,
+        "ranking": 50
+    }
+]
